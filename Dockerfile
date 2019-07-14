ace14b8b5c3a5c139e1424c0bba5d958d7e8da05
bbarton <bryanbarton525@gmail.com>
2019-07-14 19:11:18 -0400

    added pdf report generator

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM python:3.6
+
+# Install requirements
+COPY requirements.txt requirements.txt
+RUN pip3 install --upgrade pip
+RUN pip3 install -r requirements.txt
+RUN rm -rf requirements.txt
+
+# Create User
+RUN useradd tracker
+WORKDIR /home/tracker
+
+# Copy files
+COPY classes classes
+COPY configs configs
+COPY web_gui web_gui
+COPY launch_site.py launch_site.py
+
+# change ownership and run
+RUN chown -R tracker:tracker ./
+USER tracker
+
+CMD ["python", "launch_site.py"]
--- web_gui/routes.py
+++ web_gui/routes.py
@@ -1,8 +1,30 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, make_response
 from web_gui import site
 from classes.class_database import DataBase
 from web_gui.forms import LogFeeding
 from datetime import datetime
+from fpdf import FPDF, HTMLMixin
+
+
+class HTML2PDF(FPDF, HTMLMixin):
+    pass
+
+
+@site.route('/pdf', methods=['GET'])
+def jpg_to_pdf():
+    pdf = HTML2PDF(orientation='L')
+    db = DataBase()
+    results = db.get_all_records()
+    html = render_template('generate_pdf.html', results=results)
+    print(html)
+    pdf.add_page()
+    pdf.write_html(html)
+
+    response = make_response(pdf.output(dest='S').encode('latin-1'))
+    response.headers.set('Content-Disposition', 'attachment', filename='bens_feeding_report.pdf')
+    response.headers.set('Content-Type', 'application/pdf')
+
+    return response
 
 
 @site.route('/feeding_report', methods=['GET', 'POST'])
@@ -10,6 +32,7 @@ def event_report():
     db = DataBase()
     results = db.get_all_records()
     print(results)
+    db.close_connections()
 
     return render_template('bens_tracker.html', results=results)
 
@@ -32,6 +55,7 @@ def log_event():
             print(date)
             db.new_entry(start_feed=time, date=date, notes=notes)
             flash('Event Logged')
+            db.close_connections()
             return redirect(url_for('event_report'))
         if event_type == 'end_feed':
             prev_record = db.get_last_record()
@@ -39,18 +63,22 @@ def log_event():
             print(prev_record_id)
             db.new_entry(end_feed=time, id=prev_record_id, notes=notes)
             flash('Event Logged')
+            db.close_connections()
             return redirect(url_for('event_report'))
         if event_type == 'bottle':
             db.new_entry(bottle=time, date=date, notes=notes)
             flash('Event Logged')
+            db.close_connections()
             return redirect(url_for('event_report'))
         if event_type == 'poop_time':
             db.new_entry(poop_time=time, date=date, notes=notes)
             flash('Event Logged')
+            db.close_connections()
             return redirect(url_for('event_report'))
         if event_type == 'pee_time':
             db.new_entry(pee_time=time, date=date, notes=notes)
             flash('Event Logged')
+            db.close_connections()
             return redirect(url_for('event_report'))
 
     return render_template('/log_event.html', form=form)
